895ba59044cf9cc64acc2481643dc7d2566ea1f2
Swaagie <info@martijnswaagman.nl>
2015-01-06 18:42:28 +0100

    [test] render method

--- test/index.test.js
+++ test/index.test.js
@@ -1,25 +1,22 @@
 describe('Boostrap Pagelet', function () {
   'use strict';
 
-  function noop() {}
-
-  var Pagelet = require('pagelet')
-    , Bootstrapper = require('../')
+  var Bootstrapper = require('../')
+    , Pagelet = require('pagelet')
+    , Temper = require('temper')
     , assume = require('assume')
-    , temper = { fetch: function noop() { return { server: noop }}}
     , pagelet, P;
 
   beforeEach(function () {
     P = Bootstrapper.extend({
       description: 'my custom title',
-      view: 'fixtures/view.html',
       dependencies: [
         '<script src="http://code.jquery.com/jquery-2.0.0.js"></script>',
         '<script src="fixtures/custom.js"></script>"'
       ]
     });
 
-    pagelet = new P({ params: {}, temper: temper });
+    pagelet = new P({ params: {}, temper: new Temper });
   });
 
   afterEach(function each() {
@@ -37,5 +34,56 @@ describe('Boostrap Pagelet', function () {
     assume(Bootstrapper.prototype.name).to.equal('bootstrap');
     assume(Bootstrapper.prototype.name).to.equal(pagelet.name);
     assume(Bootstrapper.prototype.name).to.equal(P.prototype.name);
+  });
+
+  it('has set of default keys that will be used by #render', function () {
+    assume(pagelet.keys).to.be.an('array');
+    assume(pagelet.keys.length).to.equal(12);
+    assume(pagelet.keys).to.include('dependencies');
+    assume(pagelet.keys).to.include('_parent');
+    assume(pagelet.keys).to.include('length');
+    assume(pagelet.keys).to.include('id');
+  })
+
+  describe('#render', function () {
+    it('is a function', function () {
+      assume(pagelet.render).is.a('function');
+      assume(Pagelet.prototype.render).is.a('function');
+      assume(Bootstrapper.prototype.render.length).to.equal(0);
+    });
+
+    it('returns the initial HTML', function () {
+      var html = pagelet.render();
+
+      assume(html).to.include('<html class="no-js">');
+      assume(html).to.include('<meta charset="utf-8">');
+    });
+
+    it('uses default set of keys to replace encapsulated data', function () {
+      pagelet.keys = pagelet.keys.filter(function (key) { return key !== 'dependencies' });
+
+      assume(pagelet.render()).to.include('{dependencies}');
+    });
+
+    it('use the current values of keys', function () {
+      pagelet._parent = 'test';
+      pagelet.id = 'another id';
+
+      var html = pagelet.render();
+
+      assume(html).to.include('test');
+      assume(html).to.include('another id');
+    });
+
+    it('will use the provided view', function () {
+      pagelet = new (P.extend({
+        view: 'fixtures/view.html'
+      }).on(module))({ params: {}, temper: new Temper });
+
+      var html = pagelet.render();
+
+      assume(html).to.include('<title>Custom title</title>');
+      assume(html).to.include('<h1>Wrapping bootstrap!</h1>');
+    });
   });
 });
